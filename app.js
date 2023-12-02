0e1584b785f06ffdfae11c1be2ed94f8015f00d5
zitzmah <haleyzitzmann@gmail.com>
2023-12-01 23:53:45 -0700

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- app.js
+++ app.js
@@ -13,7 +13,24 @@ $(() => {
     const $h1 = $("<h1>")
     console.log($h1)
 
-    $h1.text('Hogwarts')
+    $h1.text("Hogwarts")
     $container.append($h1)
+
+    //*********************************** */
+    //YEAR TWO
+    //*********************************** */
+
+    const $h2 = $("<h2>").text("Haley")
+    $container.append($h2)
+
+    const $h3 = $("<h3>").text("Hufflepuff")
+    $container.append($h3)
+
+    const $h4 = $("<h4>")
+    $h4.addClass("toad").text("Trevor")
+    $container.append($h4)
+
+    const $wand = $("<h4>").text("holly wand with unicorn hair core")
+    $container.append($wand)
 });
 
