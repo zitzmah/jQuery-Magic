5e0350e9ade960ce0e971b7ea2e55dbfde32260e
zitzmah <haleyzitzmann@gmail.com>
2023-12-02 00:47:06 -0700

    Commit 3 - I added list elements. That code didn't seem very dry. I wonder if there is something I could do about it in the Hungry for More section

--- app.js
+++ app.js
@@ -32,5 +32,23 @@ $(() => {
 
     const $wand = $("<h4>").text("holly wand with unicorn hair core")
     $container.append($wand)
-});
 
+    //**************************************** */
+    //YEAR THREE
+    //**************************************** */
+    //Following what you did in previous years, add the following into an unordered list (with the attribute of storage and a value of trunk):
+    const $ul = $("<ul>").attr("storage", "trunk")
+
+    const $butterBeer = $("<li>").text("butter beer")
+    const $invisibilityCloak = $("<li>").addClass("secret").text("invisibility cloak")
+    const $magicMap = $("<li>").addClass("secret").text("magic map")
+    const $timeTurner = $("<li>").addClass("secret").text("time turner")
+    const $leash = $("<li>").addClass("toad").text("leash")
+    const $bertieBott = $("<li>").text("Bertie Bott\'s Every Flavor [Jelly] Beans")
+    $ul.append($butterBeer, $invisibilityCloak, $magicMap, $timeTurner, $leash, $bertieBott)
+    $container.append($ul)
+
+    //*************************************** */
+    //YEAR FOUR
+    //*************************************** */
+});
