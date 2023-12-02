9fee9abfdd0d6b4fccffba02d44be6399fdb9ee0
zitzmah <haleyzitzmann@gmail.com>
2023-12-01 23:18:59 -0700

    year one completed

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-
 // if (typeof jQuery == 'undefined') {
 //     console.log('oops! I still have to link my jQuery properly!');
 // } else { console.log('I did it! I linked jQuery and this js file!') };
+
+$(() => {
+    //************************** */
+    //YEAR ONE
+    //************************** */
+
+    const $container = $("#container")
+    console.log($container)
+
+    const $h1 = $("<h1>")
+    console.log($h1)
+
+    $h1.text('Hogwarts')
+    $container.append($h1)
+});
 
